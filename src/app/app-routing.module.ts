ccec7c42fd893f916142c02bbcdc421984d1afdb
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-05 01:03:50 +0200

    removed a type from interfaces.d.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { MainScreenComponent } from './main-screen/main-screen.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', component:AppComponent},
+  {path: '', component:LoginComponent},
   {path: 'imprint', component: ImprintComponent},
   {path: 'dataprotection', component: DataProtectionComponent},
   {path: 'main-page', component: MainScreenComponent},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+RouterLink
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
