3c2de9f940efd22470ba15776714419e259cc740
Philipp Mössl <pmoessl@outlook.de>
2023-10-04 18:21:45 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dabubble-60d69',
+    appId: '1:181517138663:web:ea5464f0392e0f41b8c042',
+    storageBucket: 'dabubble-60d69.appspot.com',
+    apiKey: 'AIzaSyBbnVA_fsi_fcibN_FnP-hZxlL0hrMD3Ug',
+    authDomain: 'dabubble-60d69.firebaseapp.com',
+    messagingSenderId: '181517138663',
+  },
+};
