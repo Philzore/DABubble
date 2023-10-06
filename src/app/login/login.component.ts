61df052a023bb19203eea38161c38dfbcabd0e1e
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-07 01:31:08 +0200

    added login function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 RouterLink
 @Component({
@@ -10,6 +10,26 @@ RouterLink
 })
 export class LoginComponent {
   hide = true;
-
+  email: string = '';
+  password: string = '';
+  
   constructor(private router: Router) {}
+
+  login() {
+   
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, this.email, this.password)
+      
+      .then((userCredential) => {
+        // Signed in
+        
+        console.log('User loged in successfully') 
+        const user = userCredential.user;
+        this.router.navigate(['/main-page']);
+      })
+      .catch((error) => {
+        console.log('User could not log in');
+      });
+  }
 }
