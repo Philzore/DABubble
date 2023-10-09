f4d032abe4c3722ec1d852de42e3c84b7db72ff5
Hasan Coskun <hasan.coskun38@outlook.de>
2023-10-09 19:17:37 +0200

    worked on main-chat design

--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GroupInfoPopupComponent } from '../group-info-popup/group-info-popup.component';
 import { GroupMemberComponent } from '../group-member/group-member.component';
 import { GroupAddMemberComponent } from '../group-add-member/group-add-member.component';
+import { MainThreadComponent } from '../main-thread/main-thread.component';
 
 @Component({
   selector: 'app-main-chat',
@@ -10,8 +11,15 @@ import { GroupAddMemberComponent } from '../group-add-member/group-add-member.co
   styleUrls: ['./main-chat.component.scss']
 })
 export class MainChatComponent {
-
   constructor(public dialog: MatDialog) {}
+
+  @Output() toggleComponentBEvent = new EventEmitter<boolean>();
+  showComponentB = false;
+
+  toggleComponentB() {
+    this.showComponentB = !this.showComponentB;
+    this.toggleComponentBEvent.emit(this.showComponentB);
+  }
 
   openGroupInfoPopUp(): void {
     this.dialog.open(GroupInfoPopupComponent ,{panelClass: 'custom-logout-dialog'});
@@ -24,6 +32,7 @@ export class MainChatComponent {
   openAddMemberPopUp(): void {
     this.dialog.open(GroupAddMemberComponent,{panelClass: 'custom-logout-dialog'});
   }
+
 
 
 }
