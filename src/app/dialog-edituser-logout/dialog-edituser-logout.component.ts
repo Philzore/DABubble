06f456ea576aee9608bd7048badf2326643a23f2
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-12 12:58:07 +0200

    Added User Log-Out

--- src/app/dialog-edituser-logout/dialog-edituser-logout.component.ts
+++ src/app/dialog-edituser-logout/dialog-edituser-logout.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogUserProfilComponent } from '../dialog-user-profil/dialog-user-profil.component';
-
+import { getAuth, signOut } from "firebase/auth";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-edituser-logout',
@@ -10,10 +11,23 @@ import { DialogUserProfilComponent } from '../dialog-user-profil/dialog-user-pro
 })
 export class DialogEdituserLogoutComponent {
 
- constructor(public dialogRef: MatDialogRef<DialogEdituserLogoutComponent>, public dialog:MatDialog) {}
+ constructor(public dialogRef: MatDialogRef<DialogEdituserLogoutComponent>, public dialog:MatDialog, private router: Router) {}
 
   showProfil(){
     this.dialogRef.close();
     const dialog = this.dialog.open(DialogUserProfilComponent, {position: {top:'100px',right:'50px'}, panelClass: 'custom-logout-dialog'});
+  }
+
+  logOut(){
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      this.dialogRef.close();
+      console.log('User logged out successfully')
+      this.router.navigate(['/']);
+    }).catch((error) => {
+      // An error happened.
+      console.log('User could not log out')
+    });
   }
 }
