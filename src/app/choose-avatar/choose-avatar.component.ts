d4b176b8d32c1c67d08b38db2b83dcaaa1a2e2b2
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-12 14:56:10 +0200

    Worked on choose-avatar component

--- src/app/choose-avatar/choose-avatar.component.ts
+++ src/app/choose-avatar/choose-avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-choose-avatar',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./choose-avatar.component.scss']
 })
 export class ChooseAvatarComponent {
+  @ViewChild('fileInput') fileInput: any;
 
+  openFileInput() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      // Hier kannst du die ausgewählte Datei weiterverarbeiten, z.B. hochladen oder anzeigen
+      console.log('Ausgewählte Datei:', selectedFile);
+    }
+  }
 }
