2743662a693b8b5725ee1f1fcdf806983f071387
Philipp Mössl <pmoessl@outlook.de>
2023-10-15 16:01:28 +0200

    change email in authentication

--- src/app/dialog-edit-profil/dialog-edit-profil.component.ts
+++ src/app/dialog-edit-profil/dialog-edit-profil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  EmailAuthProvider,  browserLocalPersistence, browserSessionPersistence, getAuth, onAuthStateChanged, reauthenticateWithCredential, reauthenticateWithPopup, updateEmail } from '@angular/fire/auth';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -7,6 +8,47 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./dialog-edit-profil.component.scss']
 })
 export class DialogEditProfilComponent {
+  currentUserEmail: string = '';
+  currentUserName: string = '';
+  newName: string = '';
+  newEmail: string = '';
+  auth = getAuth();
 
-  constructor(public dialogRef:MatDialogRef<DialogEditProfilComponent>) {}
+  constructor(public dialogRef: MatDialogRef<DialogEditProfilComponent>) {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    
+    const user = this.auth.currentUser;
+
+    if (user) {
+      this.currentUserEmail = user.email;
+      this.currentUserName = user.displayName;
+    } else {
+      // No user is signed in.
+    }
+  }
+
+  saveUser() {
+    
+    
+    
+    updateEmail(this.auth.currentUser, this.newEmail).then(() => {
+      // Email updated!
+      console.log('Email succesful updated to', this.newEmail);
+    }).catch((error) => {
+      // An error occurred
+      console.log('There was an error to update your email', error);
+      const provider =new EmailAuthProvider;
+      
+      reauthenticateWithPopup(this.auth.currentUser, provider).then(() => {
+        // User re-authenticated.
+      }).catch((error) => {
+        // An error ocurred
+        // ...
+      });
+
+    });
+  }
 }
--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input } from '@angular/core';
 
+import { browserLocalPersistence, getAuth, } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.scss']
 })
 export class MainScreenComponent {
-  
+
   @Input() sidebarVisible: boolean = true;
   @Input() threadVisible: boolean = true;
+
+  constructor() {
+    // const auth = getAuth();
+    // auth.setPersistence(browserLocalPersistence);
+    
+  }
 
   get isMainChatFullWidth(): boolean {
     return !this.sidebarVisible && !this.threadVisible;
