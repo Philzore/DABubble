47d909b5dff4bad75ddcdcb07b652b518c69cfde
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-16 15:57:26 +0200

    Worked on change password component ( not working yet )

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getAuth, updatePassword } from "firebase/auth";
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
+  newPassword: string = '';
+  confirmPassword: string = '';
 
+  changePassword() {
+    const auth = getAuth();
+
+    const user = auth.currentUser;
+    const newPassword = this.newPassword;
+    
+    updatePassword(user, newPassword).then(() => {
+      // Update successful.
+      console.log('Password updated successfully');
+    }).catch((error) => {
+      // An error ocurred
+      console.log(error);
+      console.log('Password could not change, error');
+    });
+  }
+
+  passwordsMatch(): boolean {
+    return this.newPassword === this.confirmPassword;
+  }
 }
+
