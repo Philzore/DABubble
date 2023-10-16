797203a26ff13b40040eb46dc5dad8dce86096b3
Philipp Mössl <pmoessl@outlook.de>
2023-10-16 20:55:26 +0200

    change reauthentication

--- src/app/dialog-edit-profil/dialog-edit-profil.component.ts
+++ src/app/dialog-edit-profil/dialog-edit-profil.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  EmailAuthProvider,  browserLocalPersistence, browserSessionPersistence, getAuth, onAuthStateChanged, reauthenticateWithCredential, reauthenticateWithPopup, updateEmail } from '@angular/fire/auth';
+import { EmailAuthProvider, OAuthProvider, browserLocalPersistence, browserSessionPersistence, getAuth, onAuthStateChanged, reauthenticateWithCredential, reauthenticateWithPopup, updateEmail } from '@angular/fire/auth';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserDataService } from '../user-data.service';
 
@@ -17,15 +17,15 @@ export class DialogEditProfilComponent {
   newEmail: string = '';
   auth = getAuth();
 
-  constructor(public dialogRef: MatDialogRef<DialogEditProfilComponent>, public userDataService:UserDataService) {
+  constructor(public dialogRef: MatDialogRef<DialogEditProfilComponent>, public userDataService: UserDataService) {
     // this.getCurrentUser();
     this.userData = userDataService.getCurrentUser();
   }
 
   getCurrentUser() {
-    
+
     const user = this.auth.currentUser;
-    
+
     if (user) {
       this.currentUserEmail = user.email;
       this.currentUserName = user.displayName;
@@ -35,23 +35,22 @@ export class DialogEditProfilComponent {
   }
 
   saveUser() {
-    
-    
-    
     updateEmail(this.auth.currentUser, this.newEmail).then(() => {
       // Email updated!
       console.log('Email succesful updated to', this.newEmail);
     }).catch((error) => {
       // An error occurred
       console.log('There was an error to update your email', error);
-      const provider =new EmailAuthProvider;
-      
-      reauthenticateWithPopup(this.auth.currentUser, provider).then(() => {
-        // User re-authenticated.
+      const provider = new OAuthProvider('google.com');
+      provider.addScope('profile');
+      provider.addScope('email');
+      reauthenticateWithPopup(this.auth.currentUser, provider).then((result) => {
+        console.log(result);
+
       }).catch((error) => {
-        // An error ocurred
-        // ...
+
       });
+
 
     });
   }
