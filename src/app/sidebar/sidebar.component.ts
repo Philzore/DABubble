ad15ce00f659fe1195e08ca3d7832210f4764ba8
Philipp Mössl <pmoessl@outlook.de>
2023-10-18 17:50:11 +0200

    add feedback fade In

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output, Injectable } from '@angular/core'; // Import Injectable
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCreateNewChannelComponent } from '../dialog-create-new-channel/dialog-create-new-channel.component';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition, sequence } from '@angular/animations';
 import { Firestore, collection, getDocs, onSnapshot } from '@angular/fire/firestore';
 import { SharedService } from '../shared.service';
 
@@ -19,8 +19,32 @@ import { SharedService } from '../shared.service';
         style({ transform: 'translateX(0)', opacity: 1 }),
         animate('500ms', style({ transform: 'translateX(-100%)', opacity: 0 }))
       ])
-    ])
-  ]
+    ]),
+    trigger("dropDownMenu", [
+      transition(":enter", [
+        style({ height: 0, overflow: "hidden" }),
+        sequence([
+          animate("200ms", style({ height: "*" })),
+        ])
+      ]),
+      transition(":leave", [
+        style({ height: "*", overflow: "hidden" }),
+        sequence([
+          animate("200ms", style({ height: 0 }))
+        ])
+      ])
+    ]),
+      trigger('fadeIn', [
+        transition(':enter', [
+          style({opacity: 0 ,transform: 'translateY(-100%)'}),
+          animate(500, style({ opacity: 1 ,transform: 'translateY(0)'}))
+        ]),
+        transition(':leave', [
+          style({ opacity: 1,transform: 'translateY(0)' }),
+          animate(500, style({ opacity: 0,transform: 'translateY(-100%)' }))
+        ])
+      ])
+    ]
 })
 export class SidebarComponent {
   @Output() sidebarToggled = new EventEmitter<boolean>();
@@ -28,12 +52,14 @@ export class SidebarComponent {
   channelDropdown: boolean = false;
   messageDropdown: boolean = false;
   sidebarClose: boolean = false;
+  activateFadeIn:boolean = true;
+  fadeInText:string = '';
   workspaceText: string = 'schließen';
   channelsFromDataBase = [];
   usersFromDatabase = [];
 
 
-  constructor(public dialog: MatDialog, private sharedService: SharedService, private firestore:Firestore) {
+  constructor(public dialog: MatDialog, private sharedService: SharedService, private firestore: Firestore) {
     this.createSubscribeChannels();
     this.createSubscribeUsers();
   }
@@ -90,5 +116,10 @@ export class SidebarComponent {
     this.sidebarClose = !this.sidebarClose;
     this.workspaceText = this.sidebarClose ? 'öffnen' : 'schließen';
     this.sharedService.toggleSidebar();
+  }
+
+  showFeedback(message:string) {
+    this.activateFadeIn = true ;
+    this.fadeInText = message ;
   }
 }
