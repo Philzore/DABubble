2c6467959d4dec84e5340fc04e2d8ca8fb4214ca
MustafaSoycan <mustafa.soycan@outlook.com>
2023-11-26 19:38:42 +0100

    Close Memberinfo is working

--- src/app/group-member-info/group-member-info.component.ts
+++ src/app/group-member-info/group-member-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-group-member-info',
@@ -9,9 +9,8 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class GroupMemberInfoComponent {
   member: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog, public dialogRef: MatDialogRef<GroupMemberInfoComponent>) {
     this.member = data.member;
   }
   
-
 }
